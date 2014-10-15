962d7922eeda05d345a0f25993136320d6500e79
Mollie Taylor <mollie.taylor@gmail.com>
2014-10-15 11:08:21 -0400

    Adds filtering by county and geolocation; improves design

--- README.md
+++ README.md
@@ -1,16 +1,33 @@
-To Do
-* Address entry 
-* Geolocation
-* Filter by date
-* Mobile app 
-* Add more counties
-* Design improvements
-* Improve mobile version
-* Add proximity viz logo and Code for ATL logo
-* Filter by county
-* Change website name
-* Adwords
-* Other publicity
+To Do:
+* Design
+	* ~~Change page title~~
+	* ~~Add Proximity Viz logo and Code for ATL logo~~
+	* ~~Improve mobile version~~
+	* Check that mobile looks OK
+	* Accessibility
+	* Design improvements (map markers, css, tour guide, map tiles)
+* Filtering
+	* ~~Filter by county~~
+	* Move county selection to separate page or div
+	* Filter by date
+* Data
+	* Add more counties
+	* Change data formatting for better filtering
+* Features
+	* Address entry 
+	* ~~Geolocation~~
+	* Mobile app 
+* Publicity
+	* Adwords
+	* Other publicity
 
-Data from 
-* http://sos.ga.gov/elections/countycontacts/advancevotingdisplay.aspx
+
+Contributors:
+* David Rohm
+* Mollie Taylor
+
+
+How you can contribute:
+* Add data for other counties from http://sos.ga.gov/elections/countycontacts/advancevotingdisplay.aspx
+* Add extra functionality
+* Improve design
--- map.js
+++ map.js
@@ -1,5 +1,18 @@
 window.onload = function () {
 
+  var hereIcon = L.icon({
+    iconUrl: 'youarehere-2.png',
+    iconSize: [32, 37],
+    iconAnchor: [16, 37],
+    popupAnchor: [0, -37]
+  });
+
+  var pollingIcon = L.AwesomeMarkers.icon({
+    icon: 'check-square-o',
+    prefix: 'fa',
+    markerColor: 'red'
+  });
+
   // Advance Voting Polling Places
 
   function onEachPolling(feature, layer) {
@@ -18,15 +31,48 @@ window.onload = function () {
       "<br>" + feature.properties.SundayTimes);
   };
 
-  var pollingLayer = L.geoJson(polling, {
-    onEachFeature: onEachPolling
+  var fultonLayer = L.geoJson(polling, {
+    onEachFeature: onEachPolling,
+    pointToLayer: function(feature, latlng) {
+      return L.marker(latlng, {icon: pollingIcon});
+    }
   }).addTo(map);
 
   // map
 
+  map.locate({
+    setView: true, 
+    maxZoom: 12,
+    watch: false
+  })
+    .on('locationfound', function(e){
+        var marker = L.marker([e.latitude, e.longitude], {icon: hereIcon})
+        // fa-bullseye or glyphicon-screenshot
+        .bindPopup('You are here');
+        map.addLayer(marker);
+        currentCoords = e.latitude;
+    })
+    .on('locationerror', function(e){
+        console.log(e);
+        alert("Location access denied.");
+    });
+
   L.tileLayer('http://otile4.mqcdn.com/tiles/1.0.0/osm/{z}/{x}/{y}.png', {
-    attribution: '<a href="http://proximityviz.com/">Proximity Viz</a> | Tiles &copy; <a href="http://www.mapquest.com/" target="_blank">MapQuest</a> <img src="http://developer.mapquest.com/content/osm/mq_logo.png" />',
+    attribution: 'Tiles &copy; <a href="http://www.mapquest.com/" target="_blank">MapQuest</a> <img src="http://developer.mapquest.com/content/osm/mq_logo.png" /> | ' +
+    'Made for <a href="http://www.codeforatlanta.org/"><img src="images/code-for-atlanta.png" height=70></a> by <a href="http://proximityviz.com/"><img src="images/prox-small.jpg"></a>',
     maxZoom: 18
+  }).addTo(map);
+
+  var counties = {
+    "Fulton": fultonLayer
+  };
+
+  var time = {
+    //
+  };
+
+  L.control.layers(counties, time, {
+    collapsed: false
   }).addTo(map);
 
 };
