9450f7d181179557739b9a6ce91ab2210a20616c
Roy Hewitt <royhewitt07@gmail.com>
2015-06-16 08:28:41 -0400

    Set county in filter service from select-county view

--- angular/js/controllers.js
+++ angular/js/controllers.js
@@ -1,12 +1,18 @@
 angular.module('earlyVotingApp')
 
-  .controller('SelectCountyCtrl', ['$scope', '$http', function ($scope, $http) {
+  .controller('SelectCountyCtrl', ['$scope', '$http', 'Filter', function ($scope, $http, Filter) {
+
   	$http.get('models/20150616.json').success(function(data) {
   		$scope.counties = data;
   	});
+
+    $scope.setCounty = function(name) {
+      Filter.setCounty(name);
+    }
   }])
 
   .controller('CountyMapCtrl', ['$scope', '$routeParams', function($scope, $routeParams){
   	$scope.countyId = $routeParams.countyId;
-  	console.log($routeParams.countyId);
+    // get county name from routeParams
+    // make sure county is set in Filter service
   }]);
