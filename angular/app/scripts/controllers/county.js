23c698f88dafe46046dc219e55edcd9504cafc75
Shawn Taylor <shawn@proximityviz.com>
2016-07-20 16:49:54 -0400

    Changes tile provider

--- angular/app/scripts/controllers/county.js
+++ angular/app/scripts/controllers/county.js
@@ -37,7 +37,7 @@ angular.module('earlyVotingApp')
 	  	angular.extend($scope, {
 	  		geojson: {},
 	  		tiles: {
-          url: "http://otile4.mqcdn.com/tiles/1.0.0/osm/{z}/{x}/{y}.png",
+          url: "http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
           options: {
             attribution: 'Tiles &copy; <a href="http://www.mapquest.com/" target="_blank">MapQuest</a> <img src="http://developer.mapquest.com/content/osm/mq_logo.png" />'
           }
