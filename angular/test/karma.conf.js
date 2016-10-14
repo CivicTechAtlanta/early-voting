f141d0d27660056b1499d21e171216c3866b59a6
Luigi Ray-Montanez <luigi.montanez@gmail.com>
2016-10-14 10:52:40 -0400

    Running grunt build wants to add these lines.  ¯\_(ツ)_/¯

--- angular/test/karma.conf.js
+++ angular/test/karma.conf.js
@@ -29,6 +29,7 @@ module.exports = function(config) {
       'bower_components/angular-simple-logger/dist/angular-simple-logger.js',
       'bower_components/leaflet/dist/leaflet-src.js',
       'bower_components/angular-leaflet-directive/dist/angular-leaflet-directive.js',
+      'bower_components/angular-messages/angular-messages.js',
       'bower_components/angular-material/angular-material.js',
       'bower_components/angular-resource/angular-resource.js',
       'bower_components/angular-route/angular-route.js',
