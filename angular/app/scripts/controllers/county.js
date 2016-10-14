a5b90992feb33015d61e60b41074ef317f3dedf5
Luigi Ray-Montanez <luigi.montanez@gmail.com>
2016-10-14 10:48:39 -0400

    Use https for OSM tiles

--- angular/app/scripts/controllers/county.js
+++ angular/app/scripts/controllers/county.js
@@ -37,7 +37,7 @@ angular.module('earlyVotingApp')
       angular.extend($scope, {
         geojson: {},
         tiles: {
-          url: "http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+          url: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
           options: {
             attribution: 'Tiles &copy; <a href="http://www.mapquest.com/" target="_blank">MapQuest</a> <img src="http://developer.mapquest.com/content/osm/mq_logo.png" />'
           }
